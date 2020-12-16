a52cba81d0f103db59131802728c77e4961d0568
Angeline Teets <angie@AngieMac.local>
2020-12-16 10:00:28 -0800

    county data, map render file and view logic

--- main_app/views.py
+++ main_app/views.py
@@ -7,3 +7,12 @@ def home(request):
 
 def about(request):
   return render(request, 'about.html')
+
+
+##This is the logic for the map page
+  # template_name = '{TEMPLATE_NAME.html}' 
+
+  # def get_context_data(self, **kwargs):
+  #     context = super(IndexView, self).get_context_data(**kwargs)
+  #     context['plot'] = map_us.render_map()
+  #     return context
