75266e486b25a957fabc20e8a2bb1e723a01b59f
DangerousKoin <kory.beiten@gmail.com>
2020-12-18 09:46:47 -0800

    glossary credits

--- main_app/urls.py
+++ main_app/urls.py
@@ -18,7 +18,6 @@ urlpatterns = [
     path('wines/<int:pk>/', views.WineDetail.as_view(), name='wines_detail'),
     path('winery/<int:winery_id>/add_wine/', views.create_wine, name='add_wine'),
 
-
     ### Grapes ###
     path('mygrapes/', views.my_grapes, name='my_grapes'),
 ]
