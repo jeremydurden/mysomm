91faa8cf15374093cb2c07f01ba51234e50aa47a
DangerousKoin <kory.beiten@gmail.com>
2020-12-17 17:11:44 -0800

    updating links and adding glossary

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,16 +5,16 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('accounts/signup/', views.signup, name='signup'),
+    path('glossary/', views.glossary, name='glossary'),
+    path('profile/', views.profile, name='profile'),
 
     ### Winery ###
     path('findwineries/', views.find_wineries, name='find_wineries'),
     path('winery/create/', views.create_winery, name='winery_create'),
     path('winery/<int:winery_id>', views.winery_detail, name="winery_detail"),
-    ### Wine ###
-    path('findwines/', views.find_wines, name='find_wines'),
+    ### Wines ###
+    path('findwines/', views.home, name='home'),
     path('mywines/', views.my_wines, name='my_wines'),
-    ### Grape ###
+    ### Grapes ###
     path('mygrapes/', views.my_grapes, name='my_grapes'),
-    ### Glossary ###
-    path('glossary/', views.GlossaryCreate.as_view(), name='glossary'),
 ]
