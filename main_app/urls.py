c4dd5861ac13f9c755252a7ec987b8215a26f195
Angeline Teets <angie@angiemac.lan>
2020-12-18 23:55:39 -0800

    search form front end and js

--- main_app/urls.py
+++ main_app/urls.py
@@ -14,13 +14,15 @@ urlpatterns = [
     path('winery/<int:winery_id>', views.winery_detail, name="winery_detail"),
     path('winery/<int:winery_id>/update/', views.winery_update, name='winery_update'),
     path('winery/<int:pk>/delete/', views.WineryDelete.as_view(), name='winery_delete'),
+    path('winery/search', views.winery_search, name="winery_search"),
     ### Wines ###
     path('findwines/', views.home, name='home'),
     path('mywines/', views.my_wines, name='my_wines'),
-    path('wines/<int:pk>/', views.WineDetail.as_view(), name='wines_detail'),
+    path('wine/<int:pk>/', views.WineDetail.as_view(), name='wine_detail'),
     path('winery/<int:winery_id>/add_wine/', views.create_wine, name='add_wine'),
     path('wine/<int:pk>/update/', views.WineUpdate.as_view(), name='wine_update'),
-    path('wine/<int:pk>/delete/', views.WineDelete.as_view(), name='wines_delete'),
+    path('wine/<int:pk>/delete/', views.WineDelete.as_view(), name='wine_delete'),
+    path('wine/search', views.wine_search, name="wine_search"),
     ### Grapes ###
     path('mygrapes/', views.my_grapes, name='my_grapes'),
 ]
